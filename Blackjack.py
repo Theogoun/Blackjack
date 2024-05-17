7b33054367d400176c089cf2a2254c42c45f6a53
Theogoun <gountheo03@gmail.com>
2024-05-17 23:12:39 +0300

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#Hand of the mother and the player
+#Hand of The House and the player
 MHand = 0
 Hand = 0
 
@@ -10,20 +10,20 @@ lost = 0                         #Indicator you lost
 #Values of cards, 0 is an ace
 #Card = np.random.randint(12)
 
-#Mother's Hand
+#The House's Hand
 Flag = 1
 MHand += np.random.randint(12)
-MHand1 = MHand                   #Mother's visible card
-MHandc = 0                       #Mother's number of hidden cards
+MHand1 = MHand                   #The House's visible card
+MHandc = 0                       #The House's number of hidden cards
 while Flag==1:
-    LC = np.random.randint(12)   #Last Card added (cause Mother cheats)
+    LC = np.random.randint(12)   #Last Card added (cause The House cheats)
     MHand += LC
     MHandc += 1
     if MHand >=21:
         MHand -= LC
         MHandc -= 1
         Flag = 0
-        print(f"Mother has: {MHand1} with {MHandc} hidden cards")
+        print(f"The House has: {MHand1} with {MHandc} hidden cards")
 
 
 #Your Hand
@@ -41,6 +41,6 @@ while Flag==1:
         Hand += np.random.randint(12)
 
 if Hand > MHand and lost == 0:
-    print(f"you won, you had {Hand}, mother had {MHand}")
+    print(f"you won, you had {Hand}, The House had {MHand}")
 else:
-    print(f"you lost, you had {Hand}, mother had {MHand}")
+    print(f"you lost, you had {Hand}, The House had {MHand}")
