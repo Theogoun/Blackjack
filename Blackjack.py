55f19a9077fdeb9f9cf7b60ef22e70fc70690209
Theogoun <gountheo03@gmail.com>
2024-05-22 00:06:01 +0300

    Now gets cards from a deck normaly

--- Blackjack.py
+++ Blackjack.py
@@ -4,22 +4,47 @@ import json
 
 D = pd.read_json("Deck.json")    #The deck
 O = np.arange(52)                #The order
+o = 0                            #Current order
 np.random.shuffle(O)             #The Shuffle
+
+lost = 0                         #Indicator you lost
+#indicator of Hit or stay is Flag
 
 #Hand of The House and the player
 MHand = 0
 Hand = 0
 
-lost = 0                         #Indicator you lost
-#indicator of Hit or stay is Flag
-
 #The House's Hand
 Flag = 1
-MHand += np.random.randint(12)
+
+#Card value start
+LC = D["Value"][O[o]]
+if LC == "Ace" and (Hand+11 > 21):
+    LC = 1
+elif LC == "Ace":
+    LC = 11
+elif isinstance(LC,int):
+    LC = D["Value"][O[o]]
+else:
+    LC = 10
+#Card value end
+
+MHand += LC
+o += 1
 MHand1 = MHand                   #The House's visible card
 MHandc = 0                       #The House's number of hidden cards
 while Flag==1:
-    LC = np.random.randint(12)   #Last Card added (cause The House cheats)
+    LC = D["Value"][O[o]]        #Last Card added
+    if LC == "Ace" and (Hand+11 > 21):
+        LC = 1
+    elif LC == "Ace":
+        LC = 11
+    elif isinstance(LC,int):
+        LC = D["Value"][O[o]]
+    else:
+        LC = 10
+    if LC == "Ace":
+        LC = 11
     MHand += LC
     MHandc += 1
     if MHand >=21:
@@ -27,11 +52,15 @@ while Flag==1:
         MHandc -= 1
         Flag = 0
         print(f"The House has: {MHand1} with {MHandc} hidden cards")
-
+    else:
+        o += 1
+if MHand == 21 and MHandc==2:
+    lost == 1
+    print("The house got Blackjack :)")
 
 #Your Hand
 Flag = 1
-while Flag==1:
+while Flag==1 and lost ==0:
     if Hand <= 21:
         print(f"you have: {Hand}")
         Flag = input("type 1 to hit and 0 to stay:")
@@ -41,7 +70,17 @@ while Flag==1:
         lost = 1
         break
     if Flag ==1:
-        Hand += np.random.randint(12)
+        LC = D["Value"][O[o]]
+        if LC == "Ace" and (Hand+11 > 21):
+            LC = 1
+        elif LC == "Ace":
+            LC = 11
+        elif isinstance(LC,int):
+            LC = D["Value"][O[o]]
+        else:
+            LC = 10
+        Hand += LC
+        o += 1
 
 if Hand > MHand and lost == 0:
     print(f"you won, you had {Hand}, The House had {MHand}")
